fbfe7390f20b9e50186a4b77be6bcd31d9ace868
Raúl Cátedra <raulcabe3003@gmail.com>
2021-06-10 16:15:49 +0200

    feat: use TDD methodologie, create getNotes test

--- src/notes/controller/notes.controller.spec.ts
+++ src/notes/controller/notes.controller.spec.ts
@@ -1,18 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { NotesService } from '../service/notes.service';
 import { NotesController } from './notes.controller';
 
 describe('NotesController', () => {
   let controller: NotesController;
 
+  const mockNoteService = {
+    getNotes: jest.fn(() => [
+      {
+        _id: '60be6969e3fb9e5fe0a6b816',
+        note: 'me encanta trabajar con NestJs',
+      },
+      { _id: '60be6969e3fb9e5fe0a6b817', note: 'hacer TDD es lo más' },
+    ]),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotesController],
-    }).compile();
+      providers: [NotesService],
+    })
+      .overrideProvider(NotesService)
+      .useValue(mockNoteService)
+      .compile();
 
     controller = module.get<NotesController>(NotesController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('http get to root should return all the notes', () => {
+    const result = [
+      {
+        _id: '60be6969e3fb9e5fe0a6b816',
+        note: 'me encanta trabajar con NestJs',
+      },
+      { _id: '60be6969e3fb9e5fe0a6b817', note: 'hacer TDD es lo más' },
+    ];
+
+    expect(controller.getNotes()).toBe(result);
   });
 });
--- src/notes/service/notes.service.spec.ts
+++ src/notes/service/notes.service.spec.ts
@@ -1,12 +1,21 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotesService } from './notes.service';
 
 describe('NotesService', () => {
   let service: NotesService;
 
+  const ModelMock = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotesService],
+      providers: [
+        NotesService,
+        {
+          provide: getModelToken('note'),
+          useValue: ModelMock,
+        },
+      ],
     }).compile();
 
     service = module.get<NotesService>(NotesService);
