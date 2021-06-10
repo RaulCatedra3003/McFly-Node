5026766bd5b56b2927c45ea87e020ad1f07c736f
Raúl Cátedra <raulcabe3003@gmail.com>
2021-06-10 17:56:40 +0200

    change bootstrap function to console.log when iniciated correctly

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  await app.listen(process.env.PORT || 4000, () => {
+    Logger.log(`Running in ${config.get('enviroment')} mode`);
+    Logger.log(`Listening on port: ${config.get('port')}`);
+  });
 }
 bootstrap();
