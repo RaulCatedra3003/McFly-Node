7bb8281ee86e08cea34bbdf679ccaaa87a343ea4
Raúl Cátedra <raulcabe3003@gmail.com>
2021-06-12 12:00:46 +0200

    feat: include pre saver hashing password in user schema

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,25 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './controller/users.controller';
 import { UserSchema } from './schema/user.schema';
 import { UsersService } from './service/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'user', schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: 'user',
+        useFactory: () => {
+          const schema = UserSchema;
+          schema.pre('save', async function () {
+            const saltRounds = 10;
+            const hashedPassword = await bcrypt.hash(this.password, saltRounds);
+            this.password = hashedPassword;
+          });
+          return schema;
+        },
+      },
+    ]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
