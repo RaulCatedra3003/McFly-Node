ac413a56390917eb1a519ce8bf462754e8014f2e
Raúl Cátedra <raulcabe3003@gmail.com>
2021-06-13 11:47:46 +0200

    fix: detele unecesary import in users service test

--- src/users/service/users.service.spec.ts
+++ src/users/service/users.service.spec.ts
@@ -1,7 +1,6 @@
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
-import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from '../dto/create-user.dtp';
 
 describe('UsersService', () => {
